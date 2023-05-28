e71c6170ff14228c35963ae6a25016a58343bfaf
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-05-28 18:19:46 -0300

    feat: create skeleton of design system

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,44 +1,54 @@
 import React, { useState } from 'react'
-import hero from '@/presentation/assets/hero-background.svg'
 import dropdownIcon from '@/presentation/assets/dropdown-icon.svg'
+import hero from '@/presentation/assets/hero-background.svg'
+import logo from '@/presentation/assets/logo.svg'
 
 import '@/presentation/pages/home/style/style.scss'
-
-import { FooterComponent } from '@/presentation/pages/home/components/footer'
-import { LogoComponent } from '@/presentation/pages/home/components/logo'
 
 export const HomePage: React.FC = () => {
   const [dropdownActive, setDropdownActive] = useState<boolean>(false);
 
   return (<>
-    <header>
-      <div className='container'>
-        <LogoComponent />
-        <div className="actions">
-          <button onClick={() => setDropdownActive(!dropdownActive)} className='dropdown-menu-btn'>
+    <header className="flex-column flex-x-center flex-y-center pv-3">
+      <div className='container vw-100 flex-row flex-x-center flex-y-around ph-2'>
+        <div className='logo-container flex-row flex-x-center flex-y-center'>
+          <img src={logo} alt='todolist icon logo with a piece of paper and a pen in the right side' />
+          <h1>My Todolist</h1>
+        </div>
+        <div className="flex-row flex-x-center flex-y-center">
+          <button onClick={() => setDropdownActive(!dropdownActive)} id='mobile-dropdown' className='btn-primary pv-1 ph-2'>
             <img src={dropdownIcon} alt='dropdown menu icon with 3 small horizontal bars' />
           </button>
-          <nav>
+          <nav id='desktop-actions'>
             <ul>
-              <li><a href='#' className='sign-in'>Sign In</a></li>
-              <li><a href='#' className='sign-up'>Sign Up</a></li>
+              <li>
+                <a href='#' className='ml-3 flex-row flex-x-center flex-y-center btn-outline-primary ph-3 pv-2'>Sign In</a>
+              </li>
+              <li>
+                <a href='#' className='flex-row flex-x-center flex-y-center btn-primary txt-c-light ph-3 pv-2 ml-2'>Sign Up</a>
+              </li>
             </ul>
           </nav>
         </div>
-        {dropdownActive && <nav>
-          <ul>
-            <li><a href='#' className='sign-in'>Sign In</a></li>
-            <li><a href='#' className='sign-up'>Sign Up</a></li>
-          </ul>
-        </nav>}
       </div>
+      {dropdownActive && <div id='mobile-actions' className='mt-3 ph-2 container vw-100 flex-row flex-x-center flex-y-around'>
+        <nav className='w-auto'>
+          <ul className='w-auto'>
+            <li className='w-auto pr-1'>
+              <a href='#' className='w-auto flex-row flex-x-center flex-y-center btn-outline-primary ph-3 pv-2'>Sign In</a>
+            </li>
+            <li className='w-auto pl-1'>
+              <a href='#' className='w-auto flex-row flex-x-center flex-y-center btn-primary txt-c-light ph-3 pv-2'>Sign Up</a>
+            </li>
+          </ul>
+        </nav>
+      </div>}
     </header>
-    <main>
-      <section>
-        <h2>Organize your daily basis with a simple and clean TODOLIST</h2>
-        <img src={hero} alt='image from a person checking their todolist tasks' />
+    <main className='vw-100'>
+      <section className='container flex-column flex-x-center flex-y-center ph-2'>
+        <h2 className='mb-6'>Organize your daily basis with a simple and clean TODOLIST</h2>
+        <img id='hero' src={hero} alt='image from a person checking their todolist tasks' />
       </section>
     </main>
-    <FooterComponent />
   </>)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,45 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  css: {
+    postcss: undefined,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          $primary: #1ca4c9;
+          $secondary: #1ac888;
+          $danger: #d32752;
+          $info: #254fba;
+          $light: #f0f0f0;
+          $dark: #212121;
+          $muted: #e9e9e9;
+
+          $xxl-padding: 2rem;
+          $xxl-margin: 2rem;
+          $xl-padding: 1.5rem;
+          $xl-margin: 1.5rem;
+          $lg-padding: 1rem;
+          $lg-margin: 1rem;
+          $base-padding: 0.75rem;
+          $base-margin: 0.75rem;
+          $sm-padding: 0.5rem;
+          $sm-margin: 0.5rem;
+          $xs-padding: 0.25rem;
+          $xs-margin: 0.25rem;
+
+          $base-border-thickness: 1px;
+          $base-border-radius: 4px;
+
+          $base-font-size: 1rem;
+
+          $xxs: 320px;
+          $xs: 600px;
+          $sm: 768px;
+          $md: 900px;
+          $lg: 1200px;
+          $xl: 1536px;
+        `
+      }
+    }
+  },
 })
