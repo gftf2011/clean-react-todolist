9ee35b14183f850c55226d4274eaa097721c2ec0
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-08-21 18:35:40 -0300

    test: create min-lenght-validation test suite

--- tests/unit/presentation/validation/validators/min-lenght-validation.spec.ts
+++ tests/unit/presentation/validation/validators/min-lenght-validation.spec.ts
@@ -1,23 +1,23 @@
 import { describe, expect, it } from 'vitest';
 
-import { MaxLengthValidation } from '@/presentation/validation/validators';
+import { MinLengthValidation } from '@/presentation/validation/validators';
 import { InvalidFieldError } from '@/presentation/validation/errors';
 
-describe('Max Lenght Validation', () => {
+describe('Min Lenght Validation', () => {
   it('GIVEN validation field is valid THEN validate with no error', () => {
     const field = 'field';
     const lenght = 1;
-    const sut = new MaxLengthValidation(field, lenght);
+    const sut = new MinLengthValidation(field, lenght);
     const input = {
       field: 'a',
     };
     expect(() => sut.validate(input)).not.toThrow();
   });
 
-  it('GIVEN validation field lenght is greater THEN must throw new "InvalidFieldError"', () => {
+  it('GIVEN validation field lenght is smaller THEN must throw new "InvalidFieldError"', () => {
     const field = 'field';
-    const lenght = 0;
-    const sut = new MaxLengthValidation(field, lenght);
+    const lenght = 2;
+    const sut = new MinLengthValidation(field, lenght);
     const input = {
       field: 'a',
     };
