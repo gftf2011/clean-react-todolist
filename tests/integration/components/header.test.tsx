8b2d127b721f0526fa8263e3afa3b95999495231
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2023-08-26 16:50:26 -0300

    test: create header default test suite

--- tests/integration/components/header.test.tsx
+++ tests/integration/components/header.test.tsx
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom';
+
 import React from 'react';
-import { describe, it, expect, afterEach } from 'vitest';
+import { describe, it, expect, afterEach, beforeEach } from 'vitest';
+
 import { cleanup, render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import {
   useLocation,
@@ -10,6 +13,8 @@ import {
 } from 'react-router-dom';
 
 import { Header } from '@/presentation/components/organisms';
+
+import { resizeScreenSize } from '@/tests/utils';
 
 type Props = {
   children: any;
@@ -65,11 +70,144 @@ const Sut: React.FC = () => {
 
 describe('FEATURE - Header Component', () => {
   describe('BACKGROUND - Desktop Screen', () => {
+    beforeEach(() => {
+      resizeScreenSize(1200);
+    });
+
     describe('SCENARIO - Logged out', () => {
       it('GIVEN page is rendered THEN display path "/"', () => {
         render(<Sut />);
         const { getByText } = within(screen.getByTestId('location-display'));
         expect(getByText('/', { exact: true })).toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered WHEN user click in sign-in link THEN redirect to "/sign-in"', async () => {
+        const app = render(<Sut />);
+
+        const listItems = app.container
+          .querySelector('#header-desktop-navigation')
+          ?.querySelector('ul')
+          ?.querySelectorAll('li') as unknown as any[];
+
+        const signInLink = listItems[1].querySelector('a');
+
+        const user = userEvent.setup();
+
+        await user.click(signInLink as HTMLElement);
+
+        const { getByText } = within(screen.getByTestId('location-display'));
+
+        expect(getByText('/sign-in', { exact: true })).toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered WHEN user click in sign-up link THEN redirect to "/sign-up"', async () => {
+        const app = render(<Sut />);
+
+        const listItems = app.container
+          .querySelector('#header-desktop-navigation')
+          ?.querySelector('ul')
+          ?.querySelectorAll('li') as unknown as any[];
+
+        const signUpLink = listItems[2].querySelector('a');
+
+        const user = userEvent.setup();
+
+        await user.click(signUpLink as HTMLElement);
+
+        const { getByText } = within(screen.getByTestId('location-display'));
+
+        expect(getByText('/sign-up', { exact: true })).toBeInTheDocument();
+      });
+
+      afterEach(() => {
+        cleanup();
+      });
+    });
+  });
+
+  describe('BACKGROUND - Mobile Screen', () => {
+    beforeEach(() => {
+      resizeScreenSize(768);
+    });
+
+    describe('SCENARIO - Logged out', () => {
+      it('GIVEN page is rendered THEN display path "/"', () => {
+        render(<Sut />);
+        const { getByText } = within(screen.getByTestId('location-display'));
+        expect(getByText('/', { exact: true })).toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered AND drop down button was not pressed THEN must not display mobile navigation item', () => {
+        const app = render(<Sut />);
+
+        const dropdownNavigation = app.container.querySelector(
+          '#header-mobile-navigation'
+        );
+
+        expect(dropdownNavigation).not.toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered AND drop down button was pressed THEN must display mobile navigation item', async () => {
+        const app = render(<Sut />);
+        const user = userEvent.setup();
+
+        const dropdownButton = app.container.querySelector(
+          '.header-mobile-dropdown-button'
+        );
+
+        await user.click(dropdownButton as HTMLElement);
+
+        const dropdownNavigation = app.container.querySelector(
+          '#header-mobile-navigation'
+        );
+
+        expect(dropdownNavigation).toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered WHEN drop down button was pressed AND user click in sign-in link THEN redirect to "/sign-in"', async () => {
+        const app = render(<Sut />);
+        const user = userEvent.setup();
+
+        const dropdownButton = app.container.querySelector(
+          '.header-mobile-dropdown-button'
+        );
+
+        await user.click(dropdownButton as HTMLElement);
+
+        const listItems = app.container
+          .querySelector('#header-mobile-navigation')
+          ?.querySelector('ul')
+          ?.querySelectorAll('li') as unknown as any[];
+        const signInLink = listItems[1].querySelector('a');
+
+        await user.click(signInLink as HTMLElement);
+
+        const { getByText } = within(screen.getByTestId('location-display'));
+
+        expect(getByText('/sign-in', { exact: true })).toBeInTheDocument();
+      });
+
+      it('GIVEN page is rendered WHEN drop down button was pressed AND user click in sign-up link THEN redirect to "/sign-up"', async () => {
+        const app = render(<Sut />);
+        const user = userEvent.setup();
+
+        const dropdownButton = app.container.querySelector(
+          '.header-mobile-dropdown-button'
+        );
+
+        await user.click(dropdownButton as HTMLElement);
+
+        const listItems = app.container
+          .querySelector('#header-mobile-navigation')
+          ?.querySelector('ul')
+          ?.querySelectorAll('li') as unknown as any[];
+        const signUpLink = listItems[2].querySelector('a');
+
+        await user.click(signUpLink as HTMLElement);
+
+        const { getByText } = within(screen.getByTestId('location-display'));
+
+        expect(getByText('/sign-up', { exact: true })).toBeInTheDocument();
       });
 
       afterEach(() => {
